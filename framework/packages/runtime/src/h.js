b3ed5ffa0afea474f3fd7c27b650ffc758506352
Karl Romet Somelar <108818570+karlromets@users.noreply.github.com>
2025-05-09 22:31:38 +0300

    feat(runtime): add mapTextNodes helper

--- framework/packages/runtime/src/h.js
+++ framework/packages/runtime/src/h.js
@@ -1,6 +1,22 @@
+import { withoutNulls } from "./utils/arrays";
 
 export const DOM_TYPES = {
-    TEXT: 'text',
-    ELEMENT: 'element',
-    FRAGMENT: 'fragment'
+  TEXT: "text",
+  ELEMENT: "element",
+  FRAGMENT: "fragment",
+};
+
+export function h(tag, props = {}, children = []) {
+  return {
+    tag,
+    props,
+    children: mapTextNodes(withoutNulls(children)),
+    type: DOM_TYPES.ELEMENT,
+  };
+}
+
+function mapTextNodes(children) {
+  return children.map((child) => {
+    typeof child === "string" ? hString(child) : child;
+  });
 }
