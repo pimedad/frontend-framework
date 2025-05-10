0a5bf985f9717ee808cdb4a74ff70fe7cdc57f56
Karl Romet Somelar <108818570+karlromets@users.noreply.github.com>
2025-05-10 13:45:35 +0300

    feat(runtime): implement mountDOM function for virtual DOM rendering

---
+++ framework/packages/runtime/src/mount-dom.js
@@ -0,0 +1,33 @@
+import { DOM_TYPES } from "./h";
+
+export function mountDOM(vdom, parentEl) {
+  switch (vdom.type) {
+    case DOM_TYPES.TEXT: {
+      createTextNode(vdom, parentEl);
+      break;
+    }
+
+    case DOM_TYPES.ELEMENT: {
+      createElementNode(vdom, parentEl);
+      break;
+    }
+
+    case DOM_TYPES.FRAGMENT: {
+      createFragmentNodes(vdom, parentEl);
+      break;
+    }
+
+    default: {
+      throw new Error(`Can't mount DOM of type: ${vdom.type}`);
+    }
+  }
+}
+
+// TODO: implement createTextNode()
+function createTextNode() {}
+
+// TODO: implement createElementNode()
+function createElementNode() {}
+
+// TODO: implement createFragmentNodes()
+function createFragmentNodes() {}
