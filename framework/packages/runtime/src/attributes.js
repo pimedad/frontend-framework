e62f79046e4fc589f57225c25a3e1194c49eb484
Karl Romet Somelar <108818570+karlromets@users.noreply.github.com>
2025-05-10 17:30:19 +0300

    feat(runtime): implement setAttribute and removeAttribute functions for managing element attributes

--- framework/packages/runtime/src/attributes.js
+++ framework/packages/runtime/src/attributes.js
@@ -36,4 +36,17 @@ export function removeStyle(el, name) {
   el.style[name] = null;
 }
 
-// TODO: implement setAttribute
+export function setAttribute(el, name, value) {
+  if (value == null) {
+    removeAttribute(el, name);
+  } else if (name.startsWith("data-")) {
+    el.setAttribute(name, value);
+  } else {
+    el[name] = value;
+  }
+}
+
+export function removeAttribute(el, name) {
+  el[name] = null;
+  el.removeAttribute(name);
+}
