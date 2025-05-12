9f3a8affb9d7194d14596c8fcb39dae01b4f5550
HolstAndreas <166335551+HolstAndreas@users.noreply.github.com>
2025-05-12 10:55:59 +0300

    feat(h): add extractChildren function of a vdom

--- framework/packages/runtime/src/h.js
+++ framework/packages/runtime/src/h.js
@@ -31,3 +31,21 @@ function mapTextNodes(children) {
     return typeof child === "string" ? hString(child) : child;
   });
 }
+
+export function extractChildren(vdom) {
+  if (vdom.children == null) {
+    return [];
+  };
+
+  const children = [];
+
+  for (const child of vdom.children) {
+    if (child.type === DOM_TYPES.FRAGMENT) {
+      children.push(...extractChildren(child, children));
+    } else {
+      children.push(child);
+    }
+  }
+
+  return children;
+}
