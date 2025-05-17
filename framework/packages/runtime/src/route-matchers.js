46927020c1f06b9dfc87ed8adc67b5abcf4a81eb
HolstAndreas <166335551+HolstAndreas@users.noreply.github.com>
2025-05-17 12:56:28 +0300

    feat(route-matcher): add route-matcher necessary for SPA

---
+++ framework/packages/runtime/src/route-matchers.js
@@ -0,0 +1,72 @@
+
+const CATCH_ALL_ROUTE = '*';
+export function makeRouteMatcher(route) {
+  return routeHasParams(route)
+    ? makeMatcherWithParams(route)
+    : makeMatcherWithoutParams(route);
+}
+
+function routeHasParams({ path }) {
+  return path.includes(':');
+}
+
+function makeRouteWithoutParamsRegex({ path }) {
+  if (path === CATCH_ALL_ROUTE) {
+    return new RegExp('^.*$');
+  };
+
+  return new RegExp(`^${path}$`);
+}
+
+function makeMatcherWithoutParams(route) {
+  const regex = makeRouteWithoutParamsRegex(route);
+  const isRedirect = typeof route.redirect === 'string';
+
+  return {
+    route,
+    isRedirect,
+    checkMatch(path) {
+      return regex.test(path);
+    },
+    extractParams() {
+      return {};
+    },
+    extractQuery,
+  }
+}
+
+function extractQuery(path) {
+  const queryIndex = path.indexOf('?');
+
+  if (queryIndex === -1) {
+    return {};
+  }
+
+  const search = new URLSearchParams(path.slice(queryIndex + 1));
+
+  return Object.fromEntries(search.entries());
+}
+
+function makeRouteWithParamsRegex({ path }) {
+  const regex = path.replace(/:([^/]+)/g, (_, paramName) => `(?<${paramName}>[^/]+)`);
+
+  return new RegExp(`^${regex}$`);
+}
+
+function makeMatcherWithParams(route) {
+  const regex = makeRouteWithParamsRegex(route);
+  const isRedirect = typeof route.redirect === 'string';
+
+  return {
+    route,
+    isRedirect,
+    checkMatch(path) {
+      return regex.test(path);
+    },
+    extractParams(path) {
+      const { groups } = regex.exec(path);
+      return groups;
+    },
+    extractQuery,
+  }
+}
